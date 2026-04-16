2b5deae452da39e48ff5196c29fa4e98a513cc75
SakuraNeeko <bryan_zac22@hotmail.com>
2026-04-15 19:42:44 -0500

    Add agregar graficarComida e iniciarJuego

--- cazando.js
+++ cazando.js
@@ -3,9 +3,13 @@ const ctx = canvas.getContext("2d");
  
 const ANCHO_GATO  = 60;
 const ALTO_GATO   = 60;
+const ANCHO_COMIDA = 30;
+const ALTO_COMIDA  = 30;
  
 let gatoX   = 0;
 let gatoY   = 0;
+let comidaX = 0;
+let comidaY = 0;
  
 function graficarRectangulo(x, y, ancho, alto, color) {
   ctx.fillStyle = color;
@@ -14,4 +18,19 @@ function graficarRectangulo(x, y, ancho, alto, color) {
 
 function graficarGato() {
   graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, "orange");
+}
+
+function graficarComida() {
+  graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, "green");
+}
+
+function iniciarJuego() {
+  gatoX = (canvas.width  - ANCHO_GATO)  / 2;
+  gatoY = (canvas.height - ALTO_GATO)   / 2;
+ 
+  comidaX = canvas.width  - ANCHO_COMIDA;
+  comidaY = canvas.height - ALTO_COMIDA;
+ 
+  graficarGato();
+  graficarComida();
 }
